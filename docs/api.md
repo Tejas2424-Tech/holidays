8a58d81b8d88c0f7bbda7aff55459b0666e99ea2
Tejas2424-Tech <tejaspatil092424@gmail.com>
2026-03-28 00:50:18 +0530

    feat: Remove numerous country-specific holiday definitions, locale files, snapshots, and related tests.

--- docs/api.md
+++
@@ -1,5 +0,0 @@
-# API Reference
-
-::: holidays.utils
-::: holidays.holiday_base
-::: holidays.ical
--- holidays/test_holidays.py
+++
@@ -1,6 +0,0 @@
-import holidays
-from datetime import date
-
-india_holidays = holidays.country_holidays('IN', years=[2026])
-print(date(2026,11,8) in india_holidays)        # True (Republic Day)
-print(india_holidays.get('2026-11-8'))         # "Republic Day"
